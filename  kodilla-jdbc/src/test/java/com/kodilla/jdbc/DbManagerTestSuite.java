2ec3180f20252a60ceddca67ac596e17fa988bca
SebastianS85 <smuk.sebastian@gmx.de>
2019-07-23 20:51:06 +0200

    12.6 v.1

---
+++  kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,76 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+
+    @Before
+    public void before(){
+        System.out.println("Starting new test case:");
+    }
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        // When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+        System.out.println("Connection ok!!!");
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+    }
+
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.ID,U.FIRSTNAME,U.LASTNAME,I.NAME,COUNT(*)AS ISSUE_QUANTITY\n" +
+                "FROM USERS U JOIN ISSUES I ON I. USER_ID_ASSIGNEDTO= U.ID\n" +
+                "GROUP BY U.ID\n" +
+                "HAVING COUNT(*) > 2" +" "
+                ;
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(  +rs.getInt("U.ID")+", "  +rs.getString("U.FIRSTNAME") + ", "+
+                    rs.getString("U.LASTNAME")+ ", " +
+                   rs.getString("ISSUE_QUANTITY"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(4, counter);
+
+    }
+}
+
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightService/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightService/Application.java
@@ -1,13 +1,20 @@
 package com.kodilla.good.patterns.flightService;
 
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
 
         FlightFinder find = new FlightFinder();
         FlightDisplayService display = new FlightDisplayService();
-        display.displayFlights(find.findDirectConnection("Gdansk", "Katowice"));
-        display.displayFlights(find.findConnectedFlights("Gdansk", "Berlin"));
+        System.out.println("Enter departure Airport");
+        Scanner airport=new Scanner(System.in);
+        String fromAirport=airport.next();
+        System.out.println("Enter arrival Airport");
+        String toAirport=airport.next();
+        display.displayFlights(find.findDirectConnection(fromAirport, toAirport));
+        display.displayFlights(find.findConnectedFlights(fromAirport, toAirport));
 
 
     }
